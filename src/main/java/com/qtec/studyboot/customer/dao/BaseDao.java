e5c0d8ce7c1077b96c8c6b6a93bb73cd90a0a527
阿杜 <duhongcaisb@163.com>
2017-11-09 13:16:20 +0800

    配置boot持久层初始化

--- src/main/java/com/qtec/studyboot/customer/dao/BaseDao.java
+++ src/main/java/com/qtec/studyboot/customer/dao/BaseDao.java
@@ -1,7 +1,43 @@
 package com.qtec.studyboot.customer.dao;
 
+import java.util.List;
+
 /**
  * Created by duhc on 2017/10/30.
  */
-public interface BaseDao {
+public interface BaseDao<T> {
+    /**
+     * 通用增加
+     *
+     * @param t
+     * @return
+     */
+    boolean insert(T t);
+
+    boolean batchInsert(List<T> batch);
+
+    /**
+     * 通用查询
+     *
+     * @param id
+     * @return
+     */
+    T findOne(int id);
+
+    /**
+     * 删除
+     *
+     * @param id
+     * @return
+     */
+    boolean deletOneById(int id);
+
+    /**
+     * 更新
+     *
+     * @param id
+     * @param t
+     * @return
+     */
+    boolean updateById(int id, T t);
 }
---
+++ src/main/java/com/qtec/studyboot/customer/dao/CustomerDao.java
@@ -0,0 +1,10 @@
+package com.qtec.studyboot.customer.dao;
+
+import com.qtec.studyboot.customer.entity.Customer;
+
+/**
+ * Created by duhc on 2017/11/9.
+ */
+public interface CustomerDao extends BaseDao<Customer>{
+
+}
