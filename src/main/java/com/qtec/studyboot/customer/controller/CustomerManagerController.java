97eed3869921823734418cd544a87530f82beb73
阿杜 <duhongcaisb@163.com>
2017-11-09 10:14:44 +0800

    配置boot日志输出

--- src/main/java/com/qtec/studyboot/customer/controller/CustomerManagerController.java
+++ src/main/java/com/qtec/studyboot/customer/controller/CustomerManagerController.java
@@ -2,6 +2,8 @@ package com.qtec.studyboot.customer.controller;
 
 import com.qtec.studyboot.customer.entity.Customer;
 import com.qtec.studyboot.customer.service.CustomerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,12 +16,13 @@ import java.util.List;
  */
 @Controller
 public class CustomerManagerController {
-
+    private static final Logger logger = LoggerFactory.getLogger(CustomerManagerController.class);
     @Autowired
     CustomerService customerService;
 
     @RequestMapping("/")
     public String index() {
+        logger.info("跳转到index页面");
         return "redirect:/list";
     }
 
@@ -27,7 +30,7 @@ public class CustomerManagerController {
     public String list(Model model) {
         List<Customer> customers = customerService.getCustomers();
         model.addAttribute("customers", customers);
-        System.out.println("获取列表");
+        logger.info("输出会员{}个",customers.size());
         // 直接到resources资源文件下templates寻找响应文件
         //next 1： 热部署
         //next 2：日志
