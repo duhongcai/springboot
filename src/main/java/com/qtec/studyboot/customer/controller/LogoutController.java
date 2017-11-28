b8f377616133583bb7fba656fd2ae9dcde1b82e0
阿杜 <duhongcaisb@163.com>
2017-11-28 19:21:09 +0800

    登出

--- src/main/java/com/qtec/studyboot/customer/controller/LogoutController.java
+++ src/main/java/com/qtec/studyboot/customer/controller/LogoutController.java
@@ -1,10 +1,24 @@
 package com.qtec.studyboot.customer.controller;
 
+import com.qtec.studyboot.customer.commons.CustomerUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by duhc on 2017/11/22.
  */
 @Controller
 public class LogoutController {
+    @RequestMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        Subject subject = SecurityUtils.getSubject();
+        subject.logout();
+        return "logout";
+    }
 }
